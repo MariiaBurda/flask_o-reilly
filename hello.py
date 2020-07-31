e018d137b5497296eea071593f681b2411f16ce3
MariiaBurda <mariiaburda93@gmail.com>
2020-07-31 16:46:01 +0300

    Added opportunity to parse command line - class Manager from flask_script

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_script import Manager
+
+
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello</h1>'
 
 
 @app.route('/user/<name>')
@@ -13,4 +17,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
